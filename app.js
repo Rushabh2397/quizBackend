3af20c7c29ec0e996645becf56ed95f0f1a8e7a3
Rushabh2397 <rushabhsancheti2397@gmail.com>
2021-10-04 21:56:43 +0530

    intial setup

---
+++ app.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import path from  'path'
+import  cookieParser from 'cookie-parser'
+
+const indexRouter = require('./routes/index');
+
+
+const app = express();
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+
+module.exports = app;
